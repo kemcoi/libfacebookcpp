0266cb962566e9f54b1e767e5f30c5d2bb7b9860
James Chou <James Chou@localhost>
2010-06-16 21:19:47 -0700

    Session: Skeleton Class

---
+++ Session.cpp
@@ -0,0 +1,23 @@
+#include "Common.h"
+#include "Session.h"
+
+
+//----------------------------------------------
+// Constructor
+Facebook::Session::Session()
+{
+	// TODO
+}
+
+//----------------------------------------------
+void Facebook::Session::Destroy()
+{
+	delete this;
+}
+
+//----------------------------------------------
+Facebook::Session::~Session()
+{
+	//TODO
+}
+

45c909f8a0445025053125bccf289ba0dddc6d33
James Chou <James Chou@localhost>
2010-06-16 20:38:24 -0700

    Commit: Add LGPL License to the headers

--- Deserializer.h
+++ Deserializer.h
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2010 
+ * Written by:
+ * Aly Hirani <alyhirani@gmail.com>
+ * James Chou <uohcsemaj@gmail.com>
+ *
+ * All Rights Reserved
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ *
+ */
+
 #ifndef FACEBOOK_DESERIALIZER_H_
 #define FACEBOOK_DESERIALIZER_H_
 
@@ -43,7 +64,7 @@ namespace Facebook
 		if(value.isNull()) \
 		throw std::exception(); \
 		\
-		if(!value.isConvertibleTo(JsonType)) \
+		if(value.isConvertibleTo(JsonType)) \
 		throw std::exception(); \
 		\
 		*t = value.ConvertFunction(); \

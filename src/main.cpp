907a7e28c19335d4b8474ac72dc900dda3974f03
Aly Hirani <Aly Hirani@localhost>
2010-06-17 22:16:06 -0400

    Adding an HTTP Request class. SSL still needs to be overrided, which I am having linking troubles with

--- src/main.cpp
+++ src/main.cpp
@@ -20,21 +20,22 @@
  */
 
 #include "Session.h"
+#include "HTTPRequest.h"
 
 #include <iostream>
 
 int main()
 {
-	Json::Reader reader;
-	Json::Value value;
+	Facebook::HttpRequest req;
+	req.SetUrl("https://graph.facebook.com/98423808305?access_token=2227470867|2.dMuycEstKb8_fvwZ1SbpoQ__.3600.1276830000-100000189955997|7AJo2z-nLZT1fZAOVgjcUhbPGl0.");
 
-	reader.parse("{   \"id\": \"98423808305\",   \"from\": {      \"name\": \"Coca-Cola\",      \"category\": \"Consumer_products\",      \"id\":\"40796308305\"   },   \"name\": \"A spectacular artwork made solely from used aluminum cans has been unveiledon top of the chalk cliffs of the Sussex coastline to mark the beginning of Recycle Week.\",  \"picture\":\"http://photos-e.ak.fbcdn.net/hphotos-ak-snc1/hs085.snc1/5041_98423808305_40796308305_1960517_6704612_.jpg\",   \"source\":\"http://sphotos.ak.fbcdn.net/hphotos-ak-snc1/hs085.snc1/5041_98423808305_40796308305_1960517_6704612_n.pg\",   \"height\": 402,   \"width\": 604,   \"link\": \"http://www.facebook.com/photo.php?pid=1960517&id=40796308305\",   \"icon\":\"http://static.ak.fbcdn.net/rsrc.php/z2E5Y/hash/8as8iqdm.gif\",   \"created_time\":\"2009-06-25T19:14:25+0000\",   \"updated_time\": \"2009-06-25T19:17:30+0000\"}", value);
+	std::auto_ptr<Json::Value> val = req.GetResponse();
 
 	Facebook::Photo *photo = new Facebook::Photo();
 
-	photo->Deserialize(value);
+	photo->Deserialize(*val);
 
-	std:: cout << curlpp::libcurlVersion() << std::endl;
+	delete photo;
 
 	return 0;
 }

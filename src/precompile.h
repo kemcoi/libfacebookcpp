84bf408225b407892947baf336483b575715cd46
Aly Hirani <Aly Hirani@localhost>
2010-06-27 15:53:49 -0400

    Making shared_ptr using in the entire library to prevent directly accessing the tr1 namespace. Adding the proper __declspec(dllimport/dllexport). Adding the header.

--- src/precompile.h
+++ src/precompile.h
@@ -1,5 +1,33 @@
+/*
+ * Copyright (C) 2010 
+ * Written by:
+ * Aly Hirani <alyhirani@gmail.com>
+ * James Chou <uohcsemaj@gmail.com>
+ *
+ * All Rights Reserved
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
 #ifndef FACEBOOK_PRECOMPILE_H_
 #define FACEBOOK_PRECOMPILE_H_
+
+// XXX: TODO: Static lib
+
+#ifdef LIBFACEBOOKCPP_EXPORTS
+#define FACEBOOK_API __declspec(dllexport)
+#else // LIBFACEBOOKCPP_EXPORTS
+#define FACEBOOK_API __declspec(dllimport)
+#endif // LIBFACEBOOKCPP_EXPORTS
 
 #define INTERFACE struct __declspec(novtable)
 #define OVERRIDE override
@@ -37,6 +65,9 @@
 #include <map>
 #include <list>
 #include <sstream>
+#include <memory>
+
+using std::tr1::shared_ptr;
 
 #include "Logger.h"
 

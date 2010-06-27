91a840f30e567e3a632d6345e73557d14fb0e49c
Aly Hirani <Aly Hirani@localhost>
2010-06-27 15:54:19 -0400

    Adding main.cpp to the examples

--- src/examples/main.cpp
+++ src/examples/main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 
+#include "Facebook.h"
+
 int main()
 {
+	{
+		// let's use this for now
+		std::string	clientid = "232486072316";
+		std::string redirectURI = "http://www.facebook.com/connect/login_success.html";
+		std::string url = Facebook::Session::GetAuthenticationURL(clientid, redirectURI, "user_agent", std::string());
+
+		std::cout << "PASTE THIS INTO YOUR BROWSER TO ACCEPT" << std::endl;
+		std::cout<<std::endl << url << std::endl;
+		std::string redirectURL;
+		system("pause"); // VS Debug
+
+		std::cout << "COPY THE REDIRECTED URL INTO HERE";
+		std::cin >> redirectURL;
+
+		std::auto_ptr<Facebook::Session> session_(Facebook::Session::Authenticate(redirectURL));
+		std::auto_ptr<const Facebook::User> user_(session_->getCurrentUser());
+
+		system("pause"); // VS Debug
+	}
+
+	return 0;
 }

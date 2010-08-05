6bff5e8ebd7e5026457c323e709f566b0cf6bd16
Aly Hirani <Aly Hirani@localhost>
2010-08-04 22:44:21 -0400

    Finishing versioning support

--- src/LibFacebookCpp.cpp
+++ src/LibFacebookCpp.cpp
@@ -1,8 +1,22 @@
 #include "precompile.hpp"
+#include "Version.h"
 
 namespace LibFacebookCpp
 {
 
+const char *GetMajorVersion()
+{
+	return LF_MAJOR;
+}
 
+const char *GetMinorVersion()
+{
+	return LF_MINOR;
+}
+
+const char *GetRevisionVersion()
+{
+	return LF_REVISION;
+}
 
 } // namespace LibFacebookCpp

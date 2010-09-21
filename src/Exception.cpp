430eb35242992edb3eda069804863cb80d51da61
Aly Hirani <Aly Hirani@localhost>
2010-09-20 23:57:05 -0400

    Re-adding back source

---
+++ src/Exception.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2010 
+ * Written by:
+ * Aly Hirani <alyhirani@gmail.com>
+ * James Chou <uohcsemaj@gmail.com>
+ *
+ * All Rights Reserved
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#include "precompile.hpp"
+#include "Exception.hpp"
+
+namespace LibFacebookCpp
+{
+
+Exception::Exception()
+{
+	// GetErrorLog() << "General Exception!";
+}
+
+InvalidArgument::InvalidArgument(const char *message) : inherited(message)
+{
+	// GetErrorLog() << message;
+}
+
+FacebookException::FacebookException(const std::string &type, const std::string &message) : inherited(type + ": " + message)
+{
+	// GetErrorLog() << type << std::endl << message;
+}
+
+NotSupportedException::NotSupportedException(const char *message) : inherited(message)
+{
+	// GetErrorLog() << message;
+}
+
+UnexpectedException::UnexpectedException(const char *message) : inherited(message)
+{
+	// GetErrorLog() << message;
+}
+
+CurlppException::CurlppException(const char *message) : inherited(message)
+{
+	// GetErrorLog() << message;
+}
+
+} // namespace LibFacebookCpp
---
+++ src/Status.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2010 
+ * Written by:
+ * Aly Hirani <alyhirani@gmail.com>
+ * James Chou <uohcsemaj@gmail.com>
+ *
+ * All Rights Reserved
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#include "precompile.hpp"
+
+#include "Deserializer.hpp"
+#include "Status.hpp"
+
+namespace LibFacebookCpp
+{
+
+//-----------------------------------------------------------
+void Status::_Deserialize(const AuthorizedObject &parent_obj, const Json::Value &json)
+{
+	Deserializer deserializer(parent_obj, this, json);
+
+	deserializer.Deserialize("id", false, &id_);
+	deserializer.Deserialize("from", false, &from_);
+	deserializer.Deserialize("message", false, &message_);
+	deserializer.Deserialize("updated_time", false, &updated_time_);
+
+}
+
+} // namespace LibFacebookCpp

cedfd134a465fad1f88903e69fddf805891ee5f0
Aly Hirani <Aly Hirani@localhost>
2010-12-28 13:59:25 -0500

    Fixes #60

--- examples/main.cpp
+++ examples/main.cpp
@@ -5,8 +5,6 @@
 
 namespace Facebook = LibFacebookCpp;
 
-#include <Windows.h>
-
 std::string GetClientId()
 {
 	std::ifstream fin("clientid.txt");
@@ -17,61 +15,57 @@ std::string GetClientId()
 
 int main()
 {
+	// let's use this for now
+	std::string	clientid = GetClientId();
+	std::string redirectURI = "http://www.facebook.com/connect/login_success.html";
+	/*Facebook::ExtendedPermissions scope;
+	scope.RequestPermission(Facebook::EP_EMAIL);
+	scope.RequestPermission(Facebook::EP_USER_PHOTOS);
+	scope.RequestPermission(Facebook::EP_READ_MAILBOX);*/
+
+	std::string url = Facebook::Session::GetAuthenticationURL(clientid, redirectURI, "user_agent", std::string());
+
+	std::cout << "PASTE THIS INTO YOUR BROWSER TO ACCEPT" << std::endl;
+	std::cout << std::endl << url << std::endl;
+	std::string redirectURL;
+
+	std::cout << "Press any key to continue...." << std::endl;
+	getch();
+
+	std::cout << "COPY THE REDIRECTED URL INTO HERE";
+	std::cin >> redirectURL;
+
+	Facebook::Session session_(redirectURL);
+
+	Facebook::User user;
+	session_.GetCurrentUser(&user);
+
+	Facebook::ResponseBlob blob;
+	user.GetPictureConnection(Facebook::PS_LARGE, &blob);
+
+	Facebook::List<Facebook::PolymorphicObject> homeList;
+	user.GetHomeConnection(&homeList);
+
+	do 
 	{
-		// let's use this for now
-		std::string	clientid = GetClientId();
-		std::string redirectURI = "http://www.facebook.com/connect/login_success.html";
-		/*Facebook::ExtendedPermissions scope;
-		scope.RequestPermission(Facebook::EP_EMAIL);
-		scope.RequestPermission(Facebook::EP_USER_PHOTOS);
-		scope.RequestPermission(Facebook::EP_READ_MAILBOX);*/
-
-		std::string url = Facebook::Session::GetAuthenticationURL(clientid, redirectURI, "user_agent", std::string());
-
-		std::cout << "PASTE THIS INTO YOUR BROWSER TO ACCEPT" << std::endl;
-		std::cout << std::endl << url << std::endl;
-		std::string redirectURL;
-		system("pause"); // VS Debug
-
-		std::cout << "COPY THE REDIRECTED URL INTO HERE";
-		std::cin >> redirectURL;
-
-		Facebook::Session session_(redirectURL);
-
-		Facebook::User user;
-		session_.GetCurrentUser(&user);
-
-		Facebook::ResponseBlob blob;
-		user.GetPictureConnection(Facebook::PS_LARGE, &blob);
-
-		Facebook::List<Facebook::PolymorphicObject> homeList;
-		user.GetHomeConnection(&homeList);
-
-		do 
-		{
-			// TODO: FIX THIS INFINITE LOOP
-			const std::list<Facebook::PolymorphicObject>& list = homeList.GetData();
-			std::cout << homeList.GetData().size() << std::endl;
-
-		} while (homeList.GetNext());
-
+		// TODO: FIX THIS INFINITE LOOP
+		const std::list<Facebook::PolymorphicObject>& list = homeList.GetData();
 		std::cout << homeList.GetData().size() << std::endl;
 
-		Facebook::List<Facebook::Album> albumList;
-		user.GetAlbumsConnection(&albumList);
+	} while (homeList.GetNext());
 
-		Facebook::List<Facebook::Message> messageInbox;
-		user.GetInboxConnection(&messageInbox);
+	std::cout << homeList.GetData().size() << std::endl;
 
-		homeList.GetNext();
+	Facebook::List<Facebook::Album> albumList;
+	user.GetAlbumsConnection(&albumList);
 
-		//HANDLE handle = CreateFile(TEXT("C:\\Users\\Aly Hirani\\Desktop\\a.jpg"), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_ARCHIVE, NULL);
-		//DWORD dw = 0;
-		//WriteFile(handle, blob.GetData(), blob.GetLength(), &dw, NULL);
-		//CloseHandle(handle);
+	Facebook::List<Facebook::Message> messageInbox;
+	user.GetInboxConnection(&messageInbox);
 
-		system("pause"); // VS Debug
-	}
+	homeList.GetNext();
+
+	std::cout << "Press any key to continue..." << std::endl;
+	getch();
 
 	return 0;
 }
